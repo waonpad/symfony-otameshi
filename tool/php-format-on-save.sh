ee2205b4dfad328ea09c74059a9d971655692cc3
waonpad <waonpad@gmail.com>
2024-11-28 03:19:15 +0900

    php-cs-fixerのvscode拡張機能を使うよう変更

--- tool/php-format-on-save.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-FILE_PATH=$1
-
-# https://github.com/PHP-CS-Fixer/PHP-CS-Fixer?tab=readme-ov-file#supported-php-versions
-PHP_CS_FIXER_IGNORE_ENV=1 ./vendor/bin/php-cs-fixer fix --config=.php-cs-fixer.dist.php $FILE_PATH
